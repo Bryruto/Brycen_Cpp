a1846513284454cc906d320d76b6439db3a9177d
Bryruto <114258995+Bryruto@users.noreply.github.com>
2026-02-18 13:19:39 -0700

    help

--- pa/pa4/functions.cpp
+++ pa/pa4/functions.cpp
@@ -1,13 +1,32 @@
 #include "functions.h"
+#include <iostream>
+#include <random>
+#include <ctime>
+#define RAND_MAX 50
 
-int getInput(std::array<float, MAX_SIZE>& ){}
+int getInput(std::array<float, MAX_SIZE>& my_arr){
+    int i{};
+    int index{0};
+    do {
+        std::cin >> i;
+        my_arr[index]= i;
+        index++;
+    } while(i != -1 && index < MAX_SIZE);
+    return static_cast<int>(my_arr.size());
+}
 
-void randInput(std::array<float, MAX_SIZE>&, int&){}
+void randInput(std::array<float, MAX_SIZE>& my_arr, int& seed){
+    std::srand(std::time(NULL));
+    int value{};
+    do{
+        
+    }while(value < MAX_SIZE);
+}
 
-float average(std::array<float, MAX_SIZE>&, int&){}
+float average(std::array<float, MAX_SIZE>& my_arr, int& ){}
 
-void maxAndMin(std::array<float, MAX_SIZE>&, int&, float& max, float& min){}
+void maxAndMin(std::array<float, MAX_SIZE>& my_arr, int&, float& max, float& min){}
 
-float standardDev(std::array<float, MAX_SIZE>&, int&){}
+float standardDev(std::array<float, MAX_SIZE>& my_arr, int&){}
 
-void display(std::array<float, MAX_SIZE>&, int&){}
+void display(std::array<float, MAX_SIZE>& my_arr, int&){}
