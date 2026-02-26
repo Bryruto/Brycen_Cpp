7aac4758a30b39135badf108c91fc7a943c139b0
Bryruto <114258995+Bryruto@users.noreply.github.com>
2026-02-25 19:33:40 -0700

    h

---
+++ pa/pa5/arrayTools.h
@@ -0,0 +1,13 @@
+#define MAX_SIZE 50
+#include <array>
+
+void generateArray(std::array<int,MAX_SIZE>& arr,int size,int min,int max);
+
+void reverseArray(std::array<int, MAX_SIZE>& arr,int size);
+
+void rotateArray(std::array<int,MAX_SIZE>& arr,int size);
+
+int findElement(std::array<int,MAX_SIZE>& arr,int size);
+
+void sortArray(std::array<int,MAX_SIZE>& arr,int size);
+
---
+++ pa/pa5/vectorTools.h
@@ -0,0 +1,7 @@
+#include <vector>
+
+
+void addElements(std::vector<int>& vec,int arr[],int size);
+
+std::vector<int> filterElements(std::vector<int>& vec,int threshold);
+
