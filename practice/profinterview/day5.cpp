9f101225d37ab3cd6f0354d887e02d344fa174a3
Bryruto <brycenez@gmail.com>
2026-04-07 21:48:00 -0600

    ol

--- practice/profinterview/day5.cpp
+++ practice/profinterview/day5.cpp
@@ -1,17 +1,15 @@
 #include <iostream>  
 
+
 int main(){
-  int arr[] = {1,2,3,4,5,7,8,99,100,-101};
+    int arr[] = {1,1,1,3,4,3,1,2,3,4,5,7,8,99,100,-101};//c sytle array with elements
 
-  int best{1};
-  int tmp{1};
-  for(size_t i{};i < 9;i++){
-    if(arr[i]+1 != arr[i+1]){
-      best = (best < tmp)? tmp: best;
-      tmp = 0;
+    int best{1};
+    size_t size = (sizeof(arr)/sizeof(arr[0]))-1;
+  
+    for(size_t i{},tmp{1};i < size;i++){
+        tmp = (arr[i] + 1 == arr[i+1])? tmp + 1 : 1;
+        best = (best < tmp)? tmp: best;
     }
-    tmp ++;
-  }
-
-  std::cout << best << std::endl;
+    std::cout << best << std::endl;
 }
